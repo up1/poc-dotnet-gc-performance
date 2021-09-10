5997d8df778ceec9fd15d8a6b0e36d5d7e359899
up1 <somkiat.p@gmail.com>
2021-09-10 09:10:40 +0700

    Poc with GC

---
+++ RedisDemo/Controllers/WeatherForecastController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using StackExchange.Redis;
+
+namespace RedisDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly IConnectionMultiplexer _redis;
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IConnectionMultiplexer redis)
+        {
+            _logger = logger;
+            _redis = redis;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetData()
+        {
+            var db = _redis.GetDatabase();
+            var data = await db.StringGetAsync("data-key");
+            return Ok(data.ToString());
+        }
+    }
+}
---
+++ RedisDemo/Startup.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Prometheus;
+using StackExchange.Redis;
+
+namespace RedisDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+
+            var multiplexer = ConnectionMultiplexer.Connect("redis");
+            services.AddSingleton<IConnectionMultiplexer>(multiplexer);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMetricServer(80, "/prometheus");
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+            app.UseHttpMetrics();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
